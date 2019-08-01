d1a1b8a085043c352a1f484da141fab0d6828a76
hellogracec <hellogracec@gmail.com>
2019-07-31 22:56:27 -0700

    test done

--- korea/scripts/menu.js
+++
@@ -1,7 +0,0 @@
-const nav = document.querySelector("header nav");
-const btn = document.querySelector(".btn-menu");
-// const nav = document.querySelector("head nav");
-
-btn.addEventListener("click", function() {
-  nav.classList.toggle("show");
-});
